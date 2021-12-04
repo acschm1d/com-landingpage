c93b3501a8c209c666da63c9689f97ce2f31cc7a
acschm1d <mail@acschmid.com>
2021-12-04 12:48:28 +0100

    Add function for generating font face rules

---
+++ src/lib/utils.js
@@ -0,0 +1,30 @@
+export const fontFace = (
+  name,
+  weight = 400,
+  style = 'normal',
+  exts = ['eot', 'woff', 'woff2', 'ttf', 'svg']
+) => {
+  const filePath = `/assets/fonts/${name.toLowerCase()}/`;
+
+  const extMods = {
+    eot: '?',
+    svg: '#' + name.replace(/ /g, '_'),
+  };
+
+  const formats = {
+    otf: 'opentype',
+    ttf: 'truetype',
+  };
+
+  let srcSet = [];
+  exts.forEach((ext) => {
+    const extmod = ext in extMods ? ext + extMods[ext] : ext;
+    const format = ext in formats ? formats[ext] : ext;
+    const fileName = weight + (style !== 'normal' && style);
+
+    srcSet.push(`url("${filePath}${fileName}.${extmod}") format("${format}")`);
+  });
+
+  const src = srcSet.join(', ');
+  return `@font-face { font-family: "${name}"; font-style: "${style}"; font-weight: ${weight}; src: ${src}; }`;
+};
