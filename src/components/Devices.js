8574d71fcff109c73c8a0c587fe71982e3a0ebb0
acschm1d <mail@acschmid.com>
2021-12-28 18:14:19 +0100

    Check for existing image before rendering

--- src/components/Devices.js
+++ src/components/Devices.js
@@ -11,9 +11,10 @@ const images = ['cup', 'laptop'].reduce((carry, image) => {
   }
 
   sizes.forEach((size) => {
+    const asset = require(`../assets/img/device-${image}-${size}.png`);
     carry[image] = [
       ...carry[image],
-      { size, img: require(`../assets/img/device-${image}-${size}.png`) },
+      { size, img: Object.entries(asset).length ? asset : null },
     ];
   });
 
@@ -57,14 +58,19 @@ const Devices = () => (
     {Object.entries(images).map(([key, imageSizePair]) => {
       return (
         <Picture key={key}>
-          {imageSizePair.map(({ img, size }) => (
-            <source
-              key={size}
-              srcSet={img.default}
-              media={`(min-width: ${size}px)`}
-            />
-          ))}
-          <img key={key} src={imageSizePair[0].img} alt="" />
+          {imageSizePair.map(
+            ({ img, size }) =>
+              img && (
+                <source
+                  key={size}
+                  srcSet={img.default}
+                  media={`(min-width: ${size}px)`}
+                />
+              )
+          )}
+          {imageSizePair[0].img && (
+            <img key={key} src={imageSizePair[0].img} alt="" />
+          )}
         </Picture>
       );
     })}
