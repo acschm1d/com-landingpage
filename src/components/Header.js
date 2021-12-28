32efe319d48c327da0073884a7cbcc41a072c6ba
acschm1d <mail@acschmid.com>
2021-12-28 17:38:57 +0100

    Adapt directory order to prevent deep nesting

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import settings from '../lib/settings';
-import { GitHub, LinkedIn } from '../assets/img/icons';
+import { GitHub, LinkedIn } from '../assets/icons';
 
 const externalLinks = [
   {
