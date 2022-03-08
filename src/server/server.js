348214fa9e02042e63417ce3541d9b755e515535
acschm1d <mail@acschmid.com>
2022-03-08 20:53:59 +0100

    Require js files with version parameter

--- src/server/server.js
+++ src/server/server.js
@@ -4,10 +4,20 @@ import express from 'express';
 import fs from 'fs';
 import path from 'path';
 import { minify } from 'html-minifier';
+import { parse } from 'node-html-parser';
 import App from '../app';
 
 const server = express();
 const port = 3000;
+
+const fileWithVersion = (fileRelativePath) => {
+  const fileStats = fs.statSync(
+    path.resolve(__dirname, `../../public${fileRelativePath}`)
+  );
+  const fileChangedTime = new Date(fileStats.mtime).getTime();
+
+  return `${fileRelativePath}?v=${fileChangedTime}`;
+};
 
 server.get(
   /\.(js|css|map|ico|webp|svg|woff|woff2|eot|ttf|webmanifest|txt)$/,
@@ -18,16 +28,31 @@ server.get('/', (_, res) => {
   const rawHTML = fs.readFileSync(path.resolve(__dirname, '../app.html'), {
     encoding: 'utf8',
   });
-  const replacedHTML = rawHTML.replace(
-    '<div id="root"></div>',
-    `<div id="root">${ReactDOMServer.renderToString(<App />)}</div>`
-  );
+  const parsedHTML = parse(rawHTML);
+
+  parsedHTML
+    .querySelectorAll('head')[0]
+    .appendChild(
+      parse(`<script src="${fileWithVersion('/build/vendor.js')}"></script>`)
+    );
+  parsedHTML
+    .querySelectorAll('body')[0]
+    .appendChild(
+      parse(`<script src="${fileWithVersion('/build/main.js')}"></script>`)
+    );
+
+  const finalHTML = parsedHTML
+    .toString()
+    .replace(
+      '<div id="root"></div>',
+      `<div id="root">${ReactDOMServer.renderToString(<App />)}</div>`
+    );
 
   res.contentType('text/html');
   res.status(200);
 
   return res.send(
-    minify(replacedHTML, {
+    minify(finalHTML, {
       collapseWhitespace: true,
       minifyCSS: true,
     })
